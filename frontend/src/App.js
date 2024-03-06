c0b33ddda4a8f169e2dab31fe88b8b6987ecaa05
unknown <anmolsaini1234>
2024-03-07 01:28:11 +0530

    Initialised

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Main from "./Pages/Main";
+import Login from "./Pages/Login"
+
+function App() {
+  return (
+    <div>
+       {/* <Main /> */}
+
+       <Login />
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Nav from '../components/nav/Nav'
+import ChatBody from '../components/chatBody/ChatBody'
+
+
+const Main = () => {
+  return (
+    <div className="__main">
+        <Nav />
+        <ChatBody />
+    </div>
+  )
+}
+
+export default Main
